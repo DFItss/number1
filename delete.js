50b208a97d4805cde145ba5bc2f71e0427c4103c
Park HyeonJun <baghyeonjun16@gmail.com>
2024-02-23 09:37:40 +0900

    PHJ_delete구현

---
+++ delete.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require('mongodb');
+
+async function del_info(table){
+  //const uri = process.env.DB_LOCAL_URL;
+  const uri = process.env.DB_ATLAS_URL;
+  const client = new MongoClient(uri);
+  //테이블 이름을 전달 받아 자동으로 그 테이블을 사용
+  try {
+    await client.connect();
+    const dbname = 'number1';
+    console.log('해당 pk값을 입력해 주세요');
+    let pk = await Input.getUserInput();
+    pk = Number(pk)
+    let pkname = `${table}_id`;
+    let qry = {}
+    qry[pkname] = pk;
+    const result = await client.db(dbname).collection(table).deleteOne(qry);
+    console.log("Document Deleted");
+  } finally {
+      await client.close();
+  }
+};
+
+module.exports ={del_info};
+
